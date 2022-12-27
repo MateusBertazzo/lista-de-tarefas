3ae9c2e3ddc8039e575720198ce8824307535969
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-12-27 15:56:25 -0300

    cria function que limpa caixa de texto quando adiciona nova tarefa

--- src/script.js
+++ src/script.js
@@ -2,11 +2,13 @@ const inputTask = document.querySelector('.input-tarefa')
 const btnTask = document.querySelector('.btn-tarefa')
 const task = document.querySelector('.tarefas')
 
+// Cria li
 const createLi = () => {
   const li = document.createElement('li');
   return li;
 };
 
+// Adiciona evento de keyPress ao pressionar ENTER
 inputTask.addEventListener('keypress', function(event) {
   if(event.keyCode === 13) {
     if(!inputTask.value) return;
@@ -14,12 +16,21 @@ inputTask.addEventListener('keypress', function(event) {
   };
 });
 
+// Limpa caixa de texto toda vez que adiciono nova tarefa
+const clearInput = () => {
+  inputTask.value = '';
+  inputTask.focus();
+};
+
+// Adiciona tarefa a OL
 const createTask = (textInput) => {
   const li = createLi();
   li.innerText = textInput;
   task.appendChild(li);
+  clearInput();
 };
 
+// Adiciona evento de click no Button
 btnTask.addEventListener('click', function(event) {
   if(!inputTask.value) return;
   createTask(inputTask.value);
