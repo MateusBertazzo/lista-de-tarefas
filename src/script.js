a42830626a995d55a4eaa01cfccbe7abfd0c8a0a
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-12-27 15:05:10 -0300

    adiciona evento de click no meu button

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const inputTask = document.querySelector('.input-tarefa')
+const btnTask = document.querySelector('.btn-tarefa')
+const task = document.querySelector('.tarefas')
+
+
+btnTask.addEventListener('click', function(event) {
+  if(!inputTask.value) return;
+});
