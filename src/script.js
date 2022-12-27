e4c65bd0f581c769d60f2c31241087d44f958e96
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-12-27 15:29:03 -0300

    adiciona event de keypress no input

--- src/script.js
+++ src/script.js
@@ -2,7 +2,27 @@ const inputTask = document.querySelector('.input-tarefa')
 const btnTask = document.querySelector('.btn-tarefa')
 const task = document.querySelector('.tarefas')
 
+const createLi = () => {
+  const li = document.createElement('li');
+  return li;
+};
+
+inputTask.addEventListener('keypress', function(event) {
+  if(event.keyCode === 13) {
+    if(!inputTask.value) return;
+    createTask(inputTask.value);
+  };
+});
+
+const createTask = (textInput) => {
+  const li = createLi();
+  li.innerText = textInput;
+  task.appendChild(li);
+};
 
 btnTask.addEventListener('click', function(event) {
   if(!inputTask.value) return;
+  createTask(inputTask.value);
 });
+
+
